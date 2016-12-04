2bb1f18895702942a20582d123c86aa8a99300ed
dev-lusaja <oscar_sanchez39@hotmail.com>
2016-12-04 17:09:03 -0500

    Add factory pattern

---
+++ factory.php
@@ -0,0 +1,85 @@
+<?php 
+
+/**
+* 
+*/
+class Party
+{
+	private $factory;
+	public $members;
+	
+	function __construct($factory)
+	{
+		$this->factory = $factory;
+		$this->members = array();
+	}
+
+	public function add_warriors($cant)
+	{
+		for ($i=0; $i < $cant; $i++) { 
+			array_push($this->members, $this->factory->create_warrior());
+		}
+	}
+
+
+	public function add_mages($cant)
+	{
+		for ($i=0; $i < $cant; $i++) { 
+			array_push($this->members, $this->factory->create_mage());
+		}
+	}
+}
+
+/**
+* 
+*/
+class HeroFactory
+{
+	
+	public function create_warrior()
+	{
+		return new Warrior();
+	}
+
+
+	public function create_mage()
+	{
+		return new Mage();
+	}
+}
+
+
+/**
+* 
+*/
+class Warrior
+{
+	
+	public $type;
+	function __construct()
+	{
+		$this->type = 'Warrior';
+	}
+}
+
+/**
+* 
+*/
+class Mage
+{
+	
+	public $type;
+	function __construct()
+	{
+		$this->type = 'Mage';
+	}
+}
+
+$party = new Party(new HeroFactory());
+$party->add_warriors(2);
+$party->add_mages(2);
+
+echo "Members: " . count($party->members) . PHP_EOL;
+var_dump($party);
+
+?>
